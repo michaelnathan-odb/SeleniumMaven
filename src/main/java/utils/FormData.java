2a3119834656125e46fdd693b2c62ed200bb2cfc
MichaelN16 <michaelnathan737@gmail.com>
2024-10-04 13:00:07 +0700

    Initial commit

---
+++ src/main/java/utils/FormData.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class FormData {
+    public String email = "michael.nathan@odb.org";
+    public String firstName = "Michael";
+    public String lastName = "Testing";
+    public String phone = "0123455678";
+    public String country = "ID";
+}
---
+++ src/test/java/pages/Subscription.java
@@ -0,0 +1,39 @@
+package pages;
+
+
+public class Subscription {
+    public static String emailBtnXpath = "//*[@id=\"row-subscription-items\"]/ul/li[2]";
+    public static String printBtnXpath = "//*[@id=\"row-subscription-items\"]/ul/li[1]";
+    public static String emailId = "email";
+    public static String firstNameId = "First Name";
+    public static String lastNameId = "Last Name";
+    public static String dropdownXpath = "/html[1]/body[1]/div[2]/div[2]/div[3]/div[2]/div[1]/div[1]/form[1]/div[1]/div[3]/select[1]";
+    public static String checkboxName = "list[]";
+    public static String submitBtnXpath = "//*[@id=\"subscribe-form-email\"]/div/button[2]";
+
+    public static void click_EmailSubs(){
+
+    }
+
+    public static void click_PrintSubs(){
+
+    }
+
+    public static void printField_all(){
+
+    }
+
+    public static void emailField_all(){
+
+    }
+
+    public static void clearPrintField(){
+
+    }
+
+    public static void clearEmailField(){
+
+    }
+
+
+}
