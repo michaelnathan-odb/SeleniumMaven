3e26033a527388ea148b29080a4bf130e9c294c6
MichaelN16 <michaelnathan737@gmail.com>
2024-10-29 17:36:12 +0700

    add TODO for some feature

---
+++ src/test/java/utils/AutomationReport.java
@@ -0,0 +1,9 @@
+package utils;
+
+public class AutomationReport {
+    static String createEmailBody() {
+        //TODO: Import scenario test to generate the list of test scenario into the email
+
+        return createEmailBody();
+    }
+}
