d735a441b934382e613aa38bc18b8defd2832d5a
michaelnathan-odb <michael.nathan@odb.org>
2024-12-09 10:26:35 +0700

    Remove use unlimited thread in pom.xml and revert the change from BrowserConfig

--- src/main/java/config/BrowserConfig.java
+++ src/main/java/config/BrowserConfig.java
@@ -3,13 +3,10 @@ package config;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.devtools.DevTools;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
-import org.openqa.selenium.firefox.FirefoxProfile;
-import org.openqa.selenium.remote.DesiredCapabilities;
 
 public class BrowserConfig {
     public static WebDriver getDriver(String browser) {
